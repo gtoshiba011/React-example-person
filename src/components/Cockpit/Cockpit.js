0719c17d417e0283122957d472ccce7cae32b95b
Howard Chen <pohaoche@usc.edu>
2020-06-01 19:05:52 -0700

    section 6 1. afd App title and send it to Cockpit

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -19,7 +19,7 @@ const Cockpit = (props) => {
   }
   return (
     <div className={cockpitCss.Cockpit}>
-      <h1>Hi, I'm a React App</h1>
+      <h1>{props.title}</h1>
       <p className={classes.join(" ")}>This is really working!</p>
       <button className={btnClass} onClick={props.doTogglePerson}>
         Show Persons
